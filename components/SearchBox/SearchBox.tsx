a60c5f8da0c8890a0463a24463747865cae38177
JustFun <paw93@ukr.net>
2026-04-01 00:10:41 +0300

    fixed searchbox

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -3,20 +3,18 @@
 import css from "./SearchBox.module.css";
 
 interface SearchBoxProps {
-  onChange: (value: string) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function SearchBox({ onChange }: SearchBoxProps) {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(event.target.value);
-  };
+ 
 
   return (
     <input
       className={css.input}
       type="text"
       placeholder="Search notes"
-      onChange={handleChange}
+      onChange={onChange}
     />
   );
 }
