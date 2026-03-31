50bc0ec57f9649beeceea250f5e5d65d522a16db
JustFun <paw93@ukr.net>
2026-04-01 02:06:40 +0300

    added verification for header link notes and  home

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,12 @@
+'use client';
 
-import css from "./Header.module.css";
-import Link from "next/link";
-import AuthNavigation from "../AuthNavigation/AuthNavigation";
+import css from './Header.module.css';
+import Link from 'next/link';
+import AuthNavigation from '../AuthNavigation/AuthNavigation';
+import { useAuthStore } from '@/lib/store/authStore';
 
 export default function Header() {
+  const isAuthenticated = useAuthStore(s => s.isAuthenticated);
   return (
     <header className={css.header}>
       <Link className={css.headerLink} href="/" aria-label="Home">
@@ -11,16 +14,17 @@ export default function Header() {
       </Link>
       <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
-          <li className={css.navigationItem}>
-            <Link className={css.navigationLink} href="/">
-              Home
-            </Link>
-          </li>
-          <li className={css.navigationItem}>
-            <Link className={css.navigationLink} href="/notes/filter/all">
-              Notes
-            </Link>
-          </li>
+          {isAuthenticated && (
+            <>
+              <li className={css.navigationItem}>
+                <Link className={css.navigationLink} href="/">Home</Link>
+              </li>
+              <li className={css.navigationItem}>
+                <Link className={css.navigationLink} href="/notes/filter/all">Notes</Link>
+              </li>
+            </>
+          )}
+
           <AuthNavigation />
         </ul>
       </nav>
